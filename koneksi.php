fea7e4dc1bcfb9bc8a72e18a64ae6baffd5ce706
Muh Raihan Resa <muhraiharesa243@gmail.com>
2024-01-20 23:48:43 +0800

    First Commit

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ReCar";
+
+$koneksi = new mysqli($host, $username, $password, $dbname);
---
+++ proses.php
@@ -0,0 +1,21 @@
+<?php
+
+include "koneksi.php";
+
+$no_ktp = $_POST['no_ktp'];
+$nama = $_POST['nama'];
+$no_hp = $_POST['no_hp'];
+$kd_mobil = $_POST['kd_mobil'];
+$tgl_peminjaman = $_POST['tgl_peminjaman'];
+$tgl_pengembalian = $_POST['tgl_pengembalian'];
+
+
+$sql = "INSERT INTO peminjaman (no_ktp, nama, no_hp, kd_mobil, tgl_peminjaman, tgl_pengembalian) VALUES ('$no_ktp','$nama','$no_hp','$kd_mobil', '$tgl_peminjaman', '$tgl_pengembalian')";
+
+(mysqli_query($koneksi, $sql));
+
+?>
+<script>
+    document.location.href = "main.php"
+    console.log("Data berhasil disimpan");
+</script>
